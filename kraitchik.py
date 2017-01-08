471731a65001cba898163fa5c71ab83cdc128e0d
Evan Roncevich <eroncev@s943348ca.ca.sandia.gov>
2017-01-07 16:10:30 -0800

    produce array of square root of primes

--- kraitchik.py
+++ kraitchik.py
@@ -26,29 +26,62 @@ def bfactor (n,arr):
             return bfactor(n / primes[i], arr)
     return None
 
+# takes in m which is a matrix counting the powers of primes ex (2^x, 3^y, 5^z, ...)
+# and counter is the base 2 counter from rref2 which adds up the matrices to get the square number
+# index is needed to add the m[index] element by default.
+def addprimes (m, counter, index):
+	tempm = m[:]
+	sum_arr = [0 for i in range (0,len(counter))]
+	sum_arr = map(lambda x,y: x+y, sum_arr, tempm[index])
+	del tempm[index]
+	for i in range(0, len(counter)):
+	    if counter[i]:
+	        sum_arr = map(lambda x,y: x+y, sum_arr, tempm[i])
+	return sum_arr
 
 # this function will go through, select one of the b+1 rows, xor with other rows, and rref2
 # the other rows. Either the rref2 will have:
 # 1. a row of all 0's, so linearly dependent ( so subset sum =0)
 # 2. all rows have 1's, so independent, (but in this case we repeat with another row which should be dependent)
 
-def subsetsum2 ( m):
+# input is nonnegative integers, and it will transform to F2
+
+def subsetsum2 ( m, pvals2):
+	
+    m2 = [] # base 2 matrix version of m
+    for i in range (0, len(m)):
+        m2.append (map (lambda x: x%2,m[i]))
+
+    #print m2
+
     for i in range (0,1):
-        print "Using m", m
+        #print "Using m2", m2
         tempm = []
-        for j in range (0, len(m)):
+        temp_pvals = pvals2[:]
+        del temp_pvals[i]
+        print "set", temp_pvals
+        for j in range (0, len(m2)):
             if i==j:
                 continue
-            tempm.append(map(lambda x,y: x ^ y , m[i],m[j]))
+            tempm.append(map(lambda x,y: x ^ y , m2[i],m2[j]))
         square = rref2 (tempm)
-        if square:
-            f
+        if square: # if the array is not empty
+            print "Found square"
+            print "base 2",square
+
+            #print "base primes", m
+            square_parr = addprimes (m, square, i)
+            print "base primes", square_parr
+            print "square", map(lambda x: x/2, square_parr)
+            exit()
         #print tempm
 
 # takes in a b*b matrix and returns the subset if 0 found or [] otherwise
+# this is just using a type of rref in base 2 which then adds up which rows were used to create
+# the final row. If the final row is all 0's, return which rows were used.
 def rref2(m):
     c=0
-    print "curr m", m
+    #print "curr m", m
     
     # create a counter which counts which elements were added to the row when turning to rref
     # This is not the LU decomposition! It simply counts which lines need to be added to make rref
@@ -59,7 +92,7 @@ def rref2(m):
         counterm[i][i] =1
 
     for i in range (0, len(m)-1):
-    	print "step m", m
+    	#print "step m", m
     	#print counterm
         if m[i][c] ==0: #first check if 1 in the correct position
             ii = find_next_row (m,i,c)
@@ -77,8 +110,8 @@ def rref2(m):
                 m[j] = map(lambda x,y: x ^ y , m[i],m[j])
                 counterm[j] = map(lambda x,y: x+y, counterm[i],counterm[j])
         c+=1
-    print m
-    print counterm
+    #print m
+    #print counterm
     if reduce(lambda x,y: x+y,m[len(m)-1],0) ==0:
         return counterm[len(m)-1]
     else:
@@ -103,13 +136,15 @@ def factor(n):
         if not bfactorable(qx):
             i+=1
             continue
+        #print qx
         parr = bfactor (qx, [0]*b)
+        print qx, parr
         pvals.append (qx)
-        pmatrix.append (map (lambda x: x%2,parr))
+        pmatrix.append (parr)#map (lambda x: x%2,parr))
         i+=1
         #print i, parr
-    #print "hi", a0
-    subsetsum2 ( pmatrix)
+    print "Table", pmatrix
+    subsetsum2 ( pmatrix, pvals)
 
 
 if len(sys.argv) < 2:

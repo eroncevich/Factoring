23a9572796eb6b2e14e000db4a5030f5adbff033
Evan Roncevich <eroncev@s943348ca.ca.sandia.gov>
2017-01-09 15:17:21 -0800

    fix random testing

--- kraitchik.py
+++ kraitchik.py
@@ -56,30 +56,26 @@ def addprimes (m, counter):
 # 2. all rows have 1's, so independent, (but in this case we repeat with another row which should be dependent)
 
 # input is nonnegative integers, and it will transform to F2
-
-def subsetsum2 ( m, pvals2):
+def subsetsum2 ( m):
 	
     m2 = [] # base 2 matrix version of m
     for i in range (0, len(m)):
         m2.append (map (lambda x: x%2,m[i]))
-
-    #print m2
+    print "M2", m2
 
     for i in range (0,len(m)):
-        #print "Using m2", m2
-        tempm = []
-        temp_pvals = pvals2[:]
-        del temp_pvals[i]
-        print "set", temp_pvals
+    	print i
+        cur_m2 = []
         for j in range (0, len(m2)):
             if i==j:
                 continue
-            tempm.append(map(lambda x,y: x ^ y , m2[i],m2[j]))
-        square = rref2 (tempm)
+            cur_m2.append(map(lambda x,y: x ^ y , m2[i],m2[j]))
+        square = rref2 (cur_m2)
         if square: # if the array is not empty, return the counter of m
             print "Found square"
             print "base 2",square
-            square.insert(i, 1)
+            square.insert(i, int(reduce(lambda x,y: x+y, square, 0) % 2))
+            print square
             return square
         
     print "error no root found"
@@ -99,26 +95,34 @@ def rref2(m):
     #print counterm
     for i in range (0, len(m)):
         counterm[i][i] =1
-
-    for i in range (0, len(m)-1):
+    i=0
+    while i < len(m)-1:
     	#print "step m", m
     	#print counterm
+    	#print c, i
+    	if c == len(m)-1:
+            return counterm[len(m)-1]
         if m[i][c] ==0: #first check if 1 in the correct position
             ii = find_next_row (m,i,c)
             if ii<0:
                 c+=1
+                continue
             else:
+                #print "switching", i, ii
                 t = m[i]
                 countert = counterm[i]
                 m[i] = m[ii]
                 counterm[i]=counterm[ii]
                 m[ii] = t
                 counterm[ii]= countert
+        if c == len(m)-1:
+            return counterm[len(m)-1]
         for j in range (i+1, len(m)):
             if m[j][c]==1:
                 m[j] = map(lambda x,y: x ^ y , m[i],m[j])
-                counterm[j] = map(lambda x,y: x+y, counterm[i],counterm[j])
+                counterm[j] = map(lambda x,y: int((x+y)%2), counterm[i],counterm[j])
         c+=1
+        i+=1
     #print m
     #print counterm
     if reduce(lambda x,y: x+y,m[len(m)-1],0) ==0:
@@ -143,16 +147,16 @@ def factor(n):
     a0 = int (math.ceil(math.sqrt (n)))
     i=a0
     while len(pvals)<b+1:
-        i = random.randint(a0,n)
+        i = random.randint(a0,n-1)
         if i in avals:
             continue
 
         qx = int ((i*i)% n)
-        #print qx
+        #print i, qx
 
         parr = bfactor (qx, [0]*b)
         if not parr:
-            i+=1
+            #i+=1
             continue
         #print qx
         avals.append (i)
@@ -160,10 +164,10 @@ def factor(n):
         print i, "->", qx, parr
         pvals.append (qx)
         pmatrix.append (parr)#map (lambda x: x%2,parr))
-        i+=1
+        #i+=1
         #print i, parr
     print "Table", pmatrix
-    square_parr = subsetsum2 ( pmatrix, pvals)
+    square_parr = subsetsum2 ( pmatrix)
     print avals
     print square_parr
     left = 1
@@ -186,13 +190,12 @@ if len(sys.argv) < 2:
     print "python bruteforce.py [integer]"
     exit()
 
-#print sys.argv[1]
-#print bfactorable (75)
-#print bfactorable (168)
-#print bfactorable (360)
-#print bfactorable (560)
-
+#random.seed(1232)
 for i in range(0,100):
     roots =factor(float(sys.argv[1]))
+    if roots[0] != 1 and roots[1] != 1:
+        print roots
+        exit()
+print "Couldn't find roots, giving up"
 #print pvals
 #print pmatrix
